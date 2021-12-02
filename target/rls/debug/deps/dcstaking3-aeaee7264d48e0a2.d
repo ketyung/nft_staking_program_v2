2d12ad68bca41565c7260e384b855bb51ea847f8
techchee <ketyung@techchee.com>
2021-12-02 20:46:33 +0800

    update the version to 0.1.2 in cargo.toml

---
+++ target/rls/debug/deps/dcstaking3-aeaee7264d48e0a2.d
@@ -0,0 +1,16 @@
+/Users/ketyung/nodejsapps/reactapp/dc/dcstaking_program/target/rls/debug/deps/dcstaking3-aeaee7264d48e0a2.rmeta: src/lib.rs src/processor.rs src/entrypoint.rs src/token_instruction.rs src/error.rs src/token_manager.rs src/state.rs src/staking_manager.rs src/index.rs src/staking_instruction.rs src/vault.rs src/vault_manager.rs
+
+/Users/ketyung/nodejsapps/reactapp/dc/dcstaking_program/target/rls/debug/deps/dcstaking3-aeaee7264d48e0a2.d: src/lib.rs src/processor.rs src/entrypoint.rs src/token_instruction.rs src/error.rs src/token_manager.rs src/state.rs src/staking_manager.rs src/index.rs src/staking_instruction.rs src/vault.rs src/vault_manager.rs
+
+src/lib.rs:
+src/processor.rs:
+src/entrypoint.rs:
+src/token_instruction.rs:
+src/error.rs:
+src/token_manager.rs:
+src/state.rs:
+src/staking_manager.rs:
+src/index.rs:
+src/staking_instruction.rs:
+src/vault.rs:
+src/vault_manager.rs:
