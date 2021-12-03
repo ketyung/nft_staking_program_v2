fe27d3586fcda62af5f1bdfec00e132fb2d1f7be
techchee <ketyung@techchee.com>
2021-12-03 08:44:28 +0800

    change version to 0.1.3 in Cargo.toml

---
+++ target/rls/debug/deps/dcstaking3-99ac73588a3b100d.d
@@ -0,0 +1,16 @@
+/Users/ketyung/nodejsapps/reactapp/dc/dcstaking_program/target/rls/debug/deps/dcstaking3-99ac73588a3b100d.rmeta: src/lib.rs src/processor.rs src/entrypoint.rs src/token_instruction.rs src/error.rs src/token_manager.rs src/state.rs src/staking_manager.rs src/index.rs src/staking_instruction.rs src/vault.rs src/vault_manager.rs
+
+/Users/ketyung/nodejsapps/reactapp/dc/dcstaking_program/target/rls/debug/deps/dcstaking3-99ac73588a3b100d.d: src/lib.rs src/processor.rs src/entrypoint.rs src/token_instruction.rs src/error.rs src/token_manager.rs src/state.rs src/staking_manager.rs src/index.rs src/staking_instruction.rs src/vault.rs src/vault_manager.rs
+
+src/lib.rs:
+src/processor.rs:
+src/entrypoint.rs:
+src/token_instruction.rs:
+src/error.rs:
+src/token_manager.rs:
+src/state.rs:
+src/staking_manager.rs:
+src/index.rs:
+src/staking_instruction.rs:
+src/vault.rs:
+src/vault_manager.rs:
